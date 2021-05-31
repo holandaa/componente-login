63434973787c083c1a74a0c85c9ccf73b7a7de43
holandaa <holandawend@gmail.com>
2021-05-30 21:50:26 -0300

    Componete Login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+
+export default class Index extends React.Component {
+  state = {
+    email: '',
+    senha: '',
+    isLoggedIn: false
+  }
+
+  handleChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  }
+
+  handleChangePassword = event => {
+    this.setState({ senha: event.target.value });
+  }
+
+  signOut = () => {
+    this.setState({ email: '' });
+    this.setState({ senha: '' });
+    this.setState({ isLoggedIn: false });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    let email = this.state.email;
+    let senha = this.state.senha;
+
+
+    axios.post(`https://projeto-integrador-4.herokuapp.com/auth/login`, { email, senha })
+      .then(res =>
+        this.setState({ isLoggedIn: (res.status === 200) })
+      ).catch(err =>
+        alert('Falha de autenticação, tente novamente!')
+      );
+  }
+
+  render() {
+    let body = (this.state.isLoggedIn)
+      ? <div>
+        <p>Connected!</p>
+        <button onClick={this.signOut}>Sair</button>
+      </div>
+      : <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+          <input type="email" name="email" onChange={this.handleChangeEmail} />
+          </label>
+          <label>
+            Senha:
+          <input type="password" name="password" onChange={this.handleChangePassword} />
+          </label>
+          <button type="submit">Acessar</button>
+        </form>
+      </div>
+
+    return (
+      body
+    )
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Index>
+  </Index>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
